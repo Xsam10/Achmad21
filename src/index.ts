5386f52da12c978526862abb79777546167535a8
Xsam10 <90443212+Xsam10@users.noreply.github.com>
2021-09-10 14:52:19 +0700

    Initialize dependabot/npm_and_yarn/typescript-4.4.2

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './api/model';
+export * from './api/Client';
+export { create } from './controllers/initializer';
+export * from '@open-wa/wa-decrypt';
+export { ev, Spin } from './controllers/events'
+export * from './utils/tools'
+export * from './structures/preProcessors'
+export * from './connect'
+//dont need to export this
+// export { getConfigWithCase } from './utils/configSchema'
+export * from './build/build-postman'
